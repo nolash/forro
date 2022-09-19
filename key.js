84579a6b11b637c8f6bf4bacb9c6af46ca68e55c
lash <dev@holbrook.no>
2022-09-19 14:44:49 +0000

    add public key submission

--- key.js
+++ key.js
@@ -7,10 +7,10 @@ async function generatePGPKey(pwd) {
 			userIDs: [{name: "Ola Nordmann", email: "ola@nordmann.no" }],
 			passphrase: pwd,
 			format: 'armored',
-			config: { rejectCurves: new Set() },
+			//config: { rejectCurves: new Set() },
 		});
 		//console.debug('pk ' + v.privateKey );
-		//console.debug('pubk ' + v.publicKey );
+		console.debug('our public key', v.publicKey );
 		localStorage.setItem('pgp-key', v.privateKey);
 		let pk = await openpgp.readKey({
 			armoredKey: v.privateKey,
@@ -38,7 +38,36 @@ async function getKey(pwd) {
 			passphrase: pwd,
 		});
 		//console.debug('pk ' + k.armor());
-		//console.debug('pubk ' + k.toPublic().armor());
+		console.debug('our public key', k.toPublic().armor());
 		whohoo(k);
 	});
-}	
+}
+
+async function generateAuth(pk, msg) {
+	let sig = await openpgp.sign({
+		signingKeys: g_local_key,
+		message: msg,
+		format: 'binary',
+		detached: true,
+	});
+	let pubkey = pk.toPublic().write();
+	let pubkey_str = String.fromCharCode.apply(null, pubkey);
+	let sig_str = String.fromCharCode.apply(null, sig);
+
+	sig_b = btoa(sig_str);
+	pub_b = btoa(pubkey_str);
+
+	return "pgp:" + pub_b + ":" + sig_b;
+}
+
+async function generatePointer(pk, pfx) {
+	let sha = new jsSHA("SHA-256", "TEXT");
+	sha.update(pfx);
+	let prefix_digest = sha.getHash("HEX");
+
+	let identity_id = pk.getFingerprint();
+	sha = new jsSHA("SHA-256", "HEX");
+	sha.update(prefix_digest);
+	sha.update(identity_id);
+	return sha.getHash("HEX");
+}
